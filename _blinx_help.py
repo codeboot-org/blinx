b787e69aec32ff64c031aab97fc2319c42f7571e
Marc Feeley <feeley@iro.umontreal.ca>
2023-06-12 18:18:21 -0400

    Fit link on single line

--- _blinx_help.py
+++ _blinx_help.py
@@ -1,3 +1,3 @@
-cmds = host_eval('CodeBoot.prototype.cb.extractCommands(window.location.href)')
+cmds = host_eval('CodeBoot.prototype.cb.extractCommands(window.location.href).filter(function (cmd) { return typeof(cmd)==="string" && cmd.startsWith("~~blinx-"); })')
 
 alert('Welcome to BLINX!\n'+repr(cmds))
