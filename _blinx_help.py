4e060bc7c22d3c272841a2141b00f8ab35621382
Marc Feeley <feeley@iro.umontreal.ca>
2023-06-13 12:28:59 -0400

    Add images and text

--- _blinx_help.py
+++ _blinx_help.py
@@ -1,3 +1,7 @@
-cmds = host_eval('CodeBoot.prototype.cb.extractCommands(window.location.href).filter(function (cmd) { return typeof(cmd)==="string" && cmd.startsWith("~~blinx-"); })')
+#cmds = host_eval('CodeBoot.prototype.cb.extractCommands(window.location.href).filter(function (cmd) { return typeof(cmd)==="string" && cmd.startsWith("~~blinx-"); })')
 
-alert('Welcome to BLINX!\n'+repr(cmds))
+html = read_file('https://raw.githubusercontent.com/codeboot-org/blinx/main/_blinx_help.html')
+
+document.body.innerHTML = html
+
+document.body.setAttribute('lang', document.querySelector('.cb-vm').getAttribute('lang'))
