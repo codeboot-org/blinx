83eb3e16808c99b2fd5e7fe633417458ae57d322
omelancon <ol.melancon@gmail.com>
2023-06-16 15:47:19 -0400

    fix codeboot eventlistener to show buttons

--- _blinx_help.py
+++ _blinx_help.py
@@ -6,7 +6,7 @@ write_file("blinx_config.py", 'blinx_id = "BLINX' + str(app.params.get('blinx-bl
 
 document.body.innerHTML = read_file('https://raw.githubusercontent.com/codeboot-org/blinx/main/_blinx_help.html')
 
-document.querySelector("#button-open-fr").setAttribute("onclick", lambda e: app.show())
-document.querySelector("#button-open-en").setAttribute("onclick", lambda e: app.show())
+document.querySelector("#button-open-fr").addEventListener("click", lambda e: app.show())
+document.querySelector("#button-open-en").addEventListener("click", lambda e: app.show())
 
 document.body.setAttribute('lang', app.lang)
