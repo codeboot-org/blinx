37de4f7c2f3ab2e2dab7dcd3d57f4c0ef425541d
Marc Feeley <feeley@iro.umontreal.ca>
2023-06-15 22:03:05 -0400

    Misplaced single quotes

--- _blinx_help.py
+++ _blinx_help.py
@@ -2,6 +2,6 @@
 
 html = read_file('https://raw.githubusercontent.com/codeboot-org/blinx/main/_blinx_help.html')
 
-host_eval('(function (html) { var body = document.querySelector("#cb-body"); CodeBoot.prototype.open = function() { body.innerHTML = ''; rte.vm.setHidden(false); }; body.innerHTML = html; })')(html)
+host_eval('(function (html) { var body = document.querySelector("#cb-body"); CodeBoot.prototype.open = function() { body.innerHTML = ""; rte.vm.setHidden(false); }; body.innerHTML = html; })')(html)
 
 document.body.setAttribute('lang', document.querySelector('.cb-vm').getAttribute('lang'))
