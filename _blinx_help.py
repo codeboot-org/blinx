2be3352e23c69dc5687914eab995470e0f6ffec7
omelancon <ol.melancon@gmail.com>
2023-06-19 12:23:37 -0400

    update blinx_help script for latest codeboot 4.0.1 release

--- _blinx_help.py
+++ _blinx_help.py
@@ -1,24 +1,33 @@
 from codeboot_app import app
 
+# Display Blinx help page
 app.hide()
-
-write_file("blinx_config.py", 'blinx_id = "BLINX' + str(app.params.get('blinx-blx', "000")) + '"')
-
 document.body.innerHTML = read_file('https://raw.githubusercontent.com/codeboot-org/blinx/main/_blinx_help.html')
 
-def add_example():
-    code = ('from chart import chart\n'
-            'from time import sleep\n'
-            '\n'
-            'blinx_id = "BLINX' + str(app.params.get('blinx-blx', "000")) + '"\n'
-            '\n'
-            'while True:\n'
-            '    data = read_file("http://" + blinx_id + ".local/temp.csv")\n'
-            '    chart(data)\n'
-            '    sleep(1)\n')
 
-    write_file("example.py", code)
-    show_file("example.py")
+# Event handlers
+def add_example():
+    blinx_config_filename = "blinx_config.py"
+    blinx_example_filename = "blinx_temp.py"
+
+    # Create file with blinx configuration from app parameters
+    blinx_config_code = ('id = "BLINX' + str(app.params.get('blinx-blx', "000")) + '"\n'
+                         'url = "http://" + blinx_id + ".local/"\n')
+    write_file(blinx_config_filename, blinx_config_code)
+
+    # Create file with blinx usage example
+    blinx_example_code = ('from chart import chart\n'
+                          'from time import sleep\n'
+                          '\n'
+                          'import blinx_config\n'
+                          '\n'
+                          'while True:\n'
+                          '    data = read_file(blinx_config.url + "temp.csv")\n'
+                          '    chart(data)\n'
+                          '    sleep(1)\n')
+
+    write_file(blinx_example_filename, blinx_example_code)
+    show_file(blinx_example_filename)
 
 def switch_to_codeboot(e):
     document.body.innerHTML = ""
@@ -26,7 +35,9 @@ def switch_to_codeboot(e):
     add_example()
     app.show()
 
+# Add event handlers
 document.querySelector("#button-open-fr").addEventListener("click", switch_to_codeboot)
 document.querySelector("#button-open-en").addEventListener("click", switch_to_codeboot)
 
+# Set language
 document.body.setAttribute('lang', app.lang)
